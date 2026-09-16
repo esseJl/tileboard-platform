059b25c4d50a6af5de141576f17296d08ef7fa5b
Esmael_Jalili <jalili.esmael@gmail.com>
2026-09-16 14:48:16 +0330

    feat(app): add RFC 7807 error handling

---
+++ tileboard-app/src/main/java/com/tileboard/app/common/exception/NoActiveGameException.java
@@ -0,0 +1,11 @@
+package com.tileboard.app.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+/** Thrown when a "stop" (or other in-game) operation is requested but no game is running. */
+public class NoActiveGameException extends ApiException {
+
+    public NoActiveGameException() {
+        super(HttpStatus.CONFLICT, "no_active_game", "No game is currently running.");
+    }
+}
